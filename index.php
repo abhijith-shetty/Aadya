8a6c8625e9981b7bd5bb98e70b237363409aef53
abhijith <abhijith_shetty@ymail.com>
2014-07-01 15:38:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 * Author: Abhijith Shetty
 * Date : 14-01-2013
 */
+//allowing Cross Domain Request
 header("Access-Control-Allow-Origin: *");
 require_once(dirname(__FILE__)."/base/autoload.class.php");
 autoload::initRest(dirname(__FILE__));
